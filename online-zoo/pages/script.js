52c404cba494546a5aed892574169f25af0d8bac
evitla <s.dinmuhamed@gmail.com>
2021-04-29 20:09:00 +0600

    feat: make watch-carousel work

--- online-zoo/pages/script.js
+++ online-zoo/pages/script.js
@@ -7,15 +7,14 @@ const darkThemeMapSrc = "../../assets/images/map-dark.svg"
 checkbox.addEventListener("change", themeToggle);
 
 function themeToggle() {
+  trans();
   if (this.checked) {
-    trans();
     document.documentElement.setAttribute("data-theme", "dark");
-    mapImg.src = darkThemeMapSrc;
-  } else {
-    trans();
-    document.documentElement.setAttribute("data-theme", "light");
-    mapImg.src = lightThemeMapSrc;
+    if (mapImg) mapImg.src = darkThemeMapSrc;
+    return;
   }
+  document.documentElement.setAttribute("data-theme", "light");
+  if (mapImg) mapImg.src = lightThemeMapSrc;
 }
 
 const trans = () => {
@@ -24,3 +23,54 @@ const trans = () => {
     document.documentElement.classList.remove("transition");
   }, 1000);
 }
+
+// =============== WATCH CAROUSEL =============== //
+const watchCarousel = document.querySelector(".watch__carousel");
+const watchCarouselSlides = watchCarousel.querySelectorAll(".carousel__slide");
+const watchInput = watchCarousel.querySelector(".watch__carousel-range");
+const watchPagination = watchCarousel.querySelector(".pagination");
+const watchPage = watchPagination.querySelector(".page");
+
+function changePage(slideNumber) {
+  console.log(page);
+  page.innerHTML = `0${slideNumber}/`;
+}
+
+let activePage = watchInput.value;
+function moveSlides(targetSlide) {
+  const carouselSlides = watchCarousel.querySelector(".carousel__slides");
+  const activeSlide = watchCarouselSlides[activePage - 1];
+  const slideWidth = targetSlide.getBoundingClientRect().width;
+  const gapProperty = window.getComputedStyle(carouselSlides).getPropertyValue("gap");
+  const gapValue = Number(gapProperty.match(/\d+/)[0]);
+
+  targetSlide.classList.add("carousel__slide_active");
+  activeSlide.classList.remove("carousel__slide_active");
+  watchPage.innerHTML = `0${targetSlide.dataset.number}/`;
+
+  // we use 2 because second slide is active by default
+  const moveAmount = (2 - targetSlide.dataset.number) * (slideWidth + gapValue);
+
+  carouselSlides.style.transform = `translateX(${moveAmount}px)`;
+  activePage = targetSlide.dataset.number;
+}
+
+function moveSlidesByInput(event) {
+  const targetInput = event.target;
+  const targetSlide = watchCarouselSlides[targetInput.value - 1];
+  
+  moveSlides(targetSlide);
+}
+
+function moveSlidesByClick(event) {
+  if (!event.target.closest("img")) return;
+  const targetSlide = event.target.parentNode;
+  if (targetSlide.classList.contains("carousel__slide_active")) return;
+
+  moveSlides(targetSlide);
+  activePage = targetSlide.dataset.number;
+  watchInput.value = activePage;
+}
+
+watchCarousel.addEventListener("input", moveSlidesByInput);
+watchCarousel.addEventListener("click", moveSlidesByClick);
