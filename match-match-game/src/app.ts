b23ea99715686b211f8b94d65e82974ce9db309d
evitla <s.dinmuhamed@gmail.com>
2021-05-25 18:46:38 +0600

    feat: add app class

---
+++ match-match-game/src/app.ts
@@ -0,0 +1,21 @@
+import { Game } from './components/game/game';
+import { ImageCategoryModel } from './models/image-category-model';
+
+export class App {
+  private readonly game: Game;
+
+  constructor(private readonly rootElement: HTMLElement) {
+    this.game = new Game();
+    this.rootElement.appendChild(this.game.element);
+  }
+
+  async start() {
+    const res = await fetch('./images.json');
+    const categories: ImageCategoryModel[] = await res.json();
+    const category = categories[0];
+    const images = category.images.map(
+      (name) => `${category.category}/${name}`
+    );
+    this.game.startGame(images);
+  }
+}
