c0d07fb9e9fa73acfe1c074f373b1c27a72c33af
evitla <s.dinmuhamed@gmail.com>
2021-05-27 14:51:10 +0600

    feat: add routing to index file

--- match-match-game/src/app.ts
+++ match-match-game/src/app.ts
@@ -6,7 +6,7 @@ import { ImageCategoryModel } from './models/image-category-model';
 export class App {
   private readonly timer: Timer;
 
-  private readonly game: Game;
+  readonly game: Game;
 
   private readonly gameWinWindow: GameWinWindow;
 
@@ -17,13 +17,13 @@ export class App {
     this.timer = new Timer();
     this.gameWinWindow = new GameWinWindow();
     this.game = new Game(this.timer, this.gameWinWindow);
-
-    this.rootElement.appendChild(this.timer.element);
-    this.rootElement.appendChild(this.game.element);
-    this.rootElement.appendChild(this.gameWinWindow.element);
   }
 
   async start() {
+    this.rootElement.appendChild(this.timer.element);
+    this.rootElement.appendChild(this.game.element);
+    this.rootElement.appendChild(this.gameWinWindow.element);
+
     const res = await fetch('./images.json');
     const categories: ImageCategoryModel[] = await res.json();
     const category = categories[0];
--- match-match-game/src/index.ts
+++ match-match-game/src/index.ts
@@ -2,12 +2,48 @@ import './styles/style.scss';
 import userImg from './assets/images/default-user-image.png';
 import { App } from './app';
 import { Header } from './components/header/header';
+import Router from './router';
+import { AboutGame } from './about-game';
 
 window.onload = () => {
   const header = new Header(userImg);
   const main = document.createElement('main');
+  const app = new App(main, header.button.element);
+  const about = new AboutGame(main);
+  about.render();
+
   document.body.appendChild(header.element);
   document.body.appendChild(main);
 
-  new App(main, header.button.element).start();
+  const router = new Router({ mode: 'hash', root: '/' });
+
+  router
+    .add(/score/, () => {
+      /* TODO */
+      app.game.stopGame();
+      header.button.element.innerText = 'Start Game';
+      main.innerText = '';
+      const title = document.createElement('h1');
+      title.innerText = 'Hello, Score!';
+      main.appendChild(title);
+    })
+    .add(/settings/, () => {
+      /* TODO */
+      app.game.stopGame();
+      header.button.element.innerText = 'Start Game';
+      main.innerText = '';
+      const title = document.createElement('h1');
+      title.innerText = 'Hello, Settings!';
+      main.appendChild(title);
+    })
+    .add(/game/, () => {
+      main.innerText = '';
+      app.start();
+    })
+    .add('', () => {
+      app.game.stopGame();
+      header.button.element.innerText = 'Start Game';
+      main.innerText = '';
+      about.render();
+    });
 };
