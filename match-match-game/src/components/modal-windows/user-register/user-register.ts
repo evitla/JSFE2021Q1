f1415b06b5f6ffb5a1c8b1ec3c18822a7832b664
evitla <s.dinmuhamed@gmail.com>
2021-05-27 14:37:06 +0600

    feat: add modal window to user registration

---
+++ match-match-game/src/components/modal-windows/user-register/user-register.ts
@@ -0,0 +1,31 @@
+import '../modal-window.scss';
+import { BaseComponent } from '../../base-component';
+import { RegistrationForm } from '../../registration-form/registration-form';
+
+export class UserRegisterWindow extends BaseComponent {
+  private readonly form: RegistrationForm;
+
+  constructor() {
+    super('div', ['modal']);
+    this.form = new RegistrationForm();
+  }
+
+  render() {
+    const contentContainer = document.createElement('div');
+    contentContainer.classList.add('modal-content');
+    const title = document.createElement('h2');
+    title.classList.add('section__title');
+    title.innerText = 'Register New Player';
+
+    this.form.render();
+
+    this.form.cancelButton.addEventListener('click', () => {
+      this.element.classList.remove('visible');
+      this.form.clear();
+    });
+
+    this.element.appendChild(contentContainer);
+    contentContainer.appendChild(title);
+    contentContainer.appendChild(this.form.element);
+  }
+}
