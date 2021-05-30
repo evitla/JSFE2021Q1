7fdab759de7a07ab5108a50448ada9ab29211c27
evitla <s.dinmuhamed@gmail.com>
2021-05-30 19:09:30 +0600

    feat: add getData method to form component

--- match-match-game/src/components/form.ts
+++ match-match-game/src/components/form.ts
@@ -1,9 +1,27 @@
+import { DatabaseRecordModel } from '../models/database-record-model';
+
 export class Form {
   element: HTMLFormElement;
+
+  data: DatabaseRecordModel | null = null;
 
   constructor(styles: string[] = []) {
     this.element = document.createElement('form');
     this.element.classList.add(...styles);
+  }
+
+  getData(event: Event): DatabaseRecordModel {
+    event.preventDefault();
+    const fd: FormData = new FormData(this.element);
+
+    this.data = {
+      name: `${fd.get('first-name')} ${fd.get('last-name')}`,
+      email: `${fd.get('email')}`,
+      password: `${fd.get('password')}`,
+      score: 0,
+    };
+
+    return this.data;
   }
 
   clear() {
