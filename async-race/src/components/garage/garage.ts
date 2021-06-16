ac0c796291cf50aee0af574148cab95f754277d7
evitla <s.dinmuhamed@gmail.com>
2021-06-16 13:00:19 +0600

    feat: add generate cars event

--- async-race/src/components/garage/garage.ts
+++ async-race/src/components/garage/garage.ts
@@ -4,6 +4,9 @@ import { Car } from '../car/car';
 import { GarageController } from '../garage-controller/garage-controller';
 import { Pagination } from '../pagination/pagination';
 import store from '../../shared/store';
+import { generateRandomCars } from './utils';
+
+const RANDOM_CARS_COUNT = 100;
 
 export class Garage extends BaseComponent {
   count: number;
@@ -16,12 +19,28 @@ export class Garage extends BaseComponent {
   ) {
     super('div', ['garage']);
 
+    this.listen();
+
+    rootElement.appendChild(this.element);
+  }
+
+  private listen() {
     this.garageController.carFormToCreate.submitButton.element.addEventListener(
       'click',
       async (event) => {
         const formData = this.garageController.carFormToCreate.getData(event);
         await this.createCar(formData);
+        await this.render();
         this.garageController.carFormToCreate.clear();
+      }
+    );
+
+    this.garageController.generateCarsButton.element.addEventListener(
+      'click',
+      async () => {
+        const cars = generateRandomCars(RANDOM_CARS_COUNT);
+        await Promise.all(cars.map((car) => this.createCar(car)));
+        await this.render();
       }
     );
 
@@ -30,8 +49,6 @@ export class Garage extends BaseComponent {
       await this.render();
       this.garageController.renderTitle(this.count);
     });
-
-    rootElement.appendChild(this.element);
   }
 
   async createCar(body: CarModel): Promise<void> {
@@ -44,8 +61,6 @@ export class Garage extends BaseComponent {
     ).json();
 
     this.count++;
-    this.garageController.renderTitle(this.count);
-    this.render();
 
     return carModel;
   }
@@ -57,7 +72,7 @@ export class Garage extends BaseComponent {
 
     this.count--;
     this.garageController.renderTitle(this.count);
-    this.render();
+    await this.render();
 
     return carModel;
   }
@@ -86,6 +101,10 @@ export class Garage extends BaseComponent {
     this.pagination.updateState(this.count);
   }
 
+  getCar = async (id: number): Promise<CarModel> => {
+    return (await fetch(`${this.url.garage}/${id}`)).json();
+  };
+
   private async getCars(
     page: number,
     limit: number
@@ -102,6 +121,7 @@ export class Garage extends BaseComponent {
 
   private renderCar(carModel: CarModel) {
     const car = new Car(carModel, this.url.engine);
+    car.render();
 
     car.selectButton.element.addEventListener('click', async () => {
       car.removeButton.element.disabled = true;
---
+++ async-race/src/components/garage/utils.ts
@@ -0,0 +1,52 @@
+import { CarModel } from '../../models/car-model';
+
+const models = [
+  'Tesla',
+  'Mercedes',
+  'BMW',
+  'Ferrari',
+  'Honda',
+  'Toyota',
+  'Ford',
+  'Volkswagen',
+  'Porsche',
+  'Bentley',
+];
+
+const names = [
+  'Model S',
+  'Camry',
+  'CLK',
+  'Combi',
+  'Cayene',
+  '7 Series',
+  '9 Series',
+  '11 Series',
+  'DB9',
+  'Model E',
+  'X5',
+];
+
+const getRandomName = () => {
+  const model = models[Math.floor(Math.random() * models.length)];
+  const name = names[Math.floor(Math.random() * names.length)];
+  return `${model} ${name}`;
+};
+
+const getRandomColor = () => {
+  const chars = '0123456789ABCDEF';
+  let color = '#';
+  for (let i = 0; i < 6; i++) {
+    color += chars[Math.floor(Math.random() * 16)];
+  }
+
+  return color;
+};
+
+export const generateRandomCars = (count = 100): CarModel[] => {
+  const result: CarModel[] = [];
+  for (let i = 0; i < count; i++) {
+    result.push({ name: getRandomName(), color: getRandomColor() });
+  }
+  return result;
+};
